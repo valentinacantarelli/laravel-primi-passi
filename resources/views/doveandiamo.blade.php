7965670584851e11eaef97934f02c3d952eca139
valentina cantarelli <valentinacanterina@gmail.com>
2021-11-04 16:02:00 +0100

    aggiunto stile

--- resources/views/doveandiamo.blade.php
+++ resources/views/doveandiamo.blade.php
@@ -5,10 +5,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Primi passi</title>
+    <style>
+        body{
+            background-image: url("https://images.unsplash.com/photo-1520643187271-06df1162815e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=772&q=80");
+            height: 88vh;
+            width: 99%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            font-family:Arial, Helvetica, sans-serif;
+        }
+        div{
+            width: 70%;
+            height: 60vh;
+            margin: 0 auto;
+            background-color: #eed9d6;
+            text-align: center;
+            padding: 20px 10px;
+            margin-top: 75px;
+        }
+        ul{
+            display:flex;
+            font-size: 16px;
+            list-style-type: none;
+            justify-content: center;
+        }
+        li{
+            padding-right: 20px;
+        }
+    </style>
 </head>
 <body>
     <div>
-        <h2>dove andiamo?</h2>
+        <h1>Dove Andiamo?</h1>
+        <ul>
+			<li><a href="/">Home Page</a></li>
+			<li><a href="/chi-siamo">Chi siamo</a></li>
+		</ul>
     </div>
 </body>
 </html>
